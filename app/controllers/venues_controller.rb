873eab5c111a2f9cc0871ea334ffbcbfb9075df0
Jorge Quinteros <jaquinterosou@gmail.com>
2026-05-25 16:31:08 -0400

    events and venues forms, new, edit

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,10 +1,51 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @venues = Venue.all
   end
 
   def show
-    @venue = Venue.find(params[:id])
     @events = @venue.events.includes(:category, :organizer)
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to @venue, notice: 'Venue was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @venue.update(venue_params)
+      redirect_to @venue, notice: 'Venue was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @venue.destroy
+    redirect_to venues_url, notice: 'Venue was successfully deleted.'
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :capacity_max)
   end
 end
